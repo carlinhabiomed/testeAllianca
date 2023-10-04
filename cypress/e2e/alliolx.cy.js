4830bbba1a4f63a84ba9803bbc8571bf7501924f
Carla Aguiar <carlinha_biomed@gmail.com>
2023-10-04 09:19:14 -0300

    outra forma

--- cypress/e2e/alliolx.cy.js
+++ cypress/e2e/alliolx.cy.js
@@ -3,7 +3,7 @@ describe('Teste Alliança - OlX', () => {
 
     beforeEach(() => {
         cy.visit('/')
-
+        cy.get('#cookie-notice-ok-button').click()
     });
 
     afterEach(() => {
@@ -13,10 +13,10 @@ describe('Teste Alliança - OlX', () => {
 
 
     it('pesquisa válida', () => {
-       cy.fixture('itensPesquisados.json').then((data) => {
+        cy.fixture('itensPesquisados.json').then((data) => {
             cy.realizaBusca(data.itemValido)
             cy.get('h1').should('contain.text', data.itemValido);
-            
+
 
         })
 
@@ -24,13 +24,34 @@ describe('Teste Alliança - OlX', () => {
 
     it('pesquisa inválida', () => {
         cy.fixture('itensPesquisados.json').then((data) => {
-             cy.realizaBusca(data.itemInvalido)
-             cy.get('#main-content > div > span').should('contain.text', "Ops! Nenhum anúncio foi encontrado.");
-             
- 
-         })
- 
-     });
+            cy.realizaBusca(data.itemInvalido)
+            cy.get('#main-content > div > span').should('contain.text', "Ops! Nenhum anúncio foi encontrado.");
 
+
+        })
+
+    })
+    it('OLX busca válida 2', () => {
+        cy.fixture('itensPesquisados.json').then((data) => {
+            //cy.get('[data-testid="oraculo-4-input"]').should('be.visible').type(`${data.itemValido}{enter}`)
+            cy.visit(`/estado-sp?q=${data.itemValido}`).then(() => {
+
+                cy.get('h1').should('contain.text', data.itemValido);
+            });
+
+        })
+    })
+
+    it('OLX busca inválida 2', () => {
+        cy.fixture('itensPesquisados.json').then((data) => {
+            //cy.get('[data-testid="oraculo-4-input"]').should('be.visible').type(`${data.itemInvalido}{enter}`)
+            cy.visit(`/estado-sp?q=` + data.itemInvalido).then(() => {
+
+                cy.get('#main-content > div > span').should('contain.text', "Ops! Nenhum anúncio foi encontrado.");
+            })
+        })
+
+
+    });
 });
 
